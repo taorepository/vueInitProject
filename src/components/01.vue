e439f41728d4dc46b68c40f99cccf0f4301e566c
wang taotao <898611810@qq.com>
2019-07-17 18:19:41 +0800

    路由跳转demo

---
+++ src/components/01.vue
@@ -0,0 +1,46 @@
+<template>
+  <todo-list :todos="todos">
+    <template slot-scope="s1">
+      <span v-if="s1.todo.isComplete">✓</span>
+      <span v-else>x</span>
+      <span>{{s1.todo.text}}</span>
+    </template>
+  </todo-list>
+</template>
+
+<script>
+import todoList from './02.vue'
+export default {
+  data () {
+    return {
+      todos: [
+        {
+          id: 0,
+          text: 'ziwei0',
+          isComplete: false
+        },
+        {
+          text: 'ziwei1',
+          id: 1,
+          isComplete: true
+        },
+        {
+          text: 'ziwei2',
+          id: 2,
+          isComplete: false
+        },
+        {
+          text: 'ziwei3',
+          id: 3,
+          isComplete: false
+        }
+      ]
+    }
+  },
+
+  components: {
+    todoList
+  }
+
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import T02 from '@/components/01'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
-      path: '/',
+      path: '/01',
       name: 'HelloWorld',
+      component: T02
+    },
+    {
+      path: '/02',
+      name: 'h1',
       component: HelloWorld
     }
   ]
